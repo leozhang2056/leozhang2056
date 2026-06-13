5b50712cbc96f3c8ab41d9a7eb76884791ca2d6a
leozhang <10288369+leozhang2056@users.noreply.github.com>
2026-06-13 23:06:14 +1200

    Portfolio icon: briefcase → globe (more eye-catching)

--- app/backend/generate_cv_from_kb.py
+++ app/backend/generate_cv_from_kb.py
@@ -3734,7 +3734,7 @@ _CV_ICON_GITHUB_SVG = (
 _CV_ICON_PORTFOLIO_SVG = (
     '<svg class="cv-header-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" '
     'aria-hidden="true" focusable="false">'
-    '<path fill="currentColor" d="M20 6h-4V4c0-1.1-.9-2-2-2h-4c-1.1 0-2 .9-2 2v2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zM10 4h4v2h-4V4zm10 16H4V8h16v12z"/>'
+    '<path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>'
     '</svg>'
 )
 
